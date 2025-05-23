ea33e6de5605815a4e9825c11be448c3356fc00a
Kim Toston <ritztoston@gmail.com>
2025-05-23 09:30:06 +0800

    Hopefully fix build

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'class'
   theme: {
     extend: {
       fontFamily: {
